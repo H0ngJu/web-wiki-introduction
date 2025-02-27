e9b2941089f8caafa0beabf9e0ef9c2b608ce0ab
H0ngJu <hongju4385@gmail.com>
2025-02-27 20:39:16 +0900

    자기소개 위키 완성

--- js/index.js
+++ js/index.js
@@ -1,19 +1,24 @@
-console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
-);
+const registerButton = document.getElementById(`register-button`);
+const textArea = document.getElementById("input-text");
+const commentList = document.querySelector(".comment-list");
+registerButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (textArea.value) {
+    const newComment = document.createElement("li");
+    newComment.innerHTML = `
+      <div class="comment-item">
+        <div class="comment-author">
+          <img
+            src="./images/comment-author-icon.png"
+            alt="사용자 프로필 이미지"
+          />
+          <span>방문자</span>
+        </div>
+        <div class="comment-content">${textArea.value}</div>
+      </div>`;
 
-console.log("알맞은 스크립트를 작성하세요");
+    commentList.prepend(newComment);
+
+    textArea.value = "";
+  }
+});
